7796e11852df54984d56460b56bf8d3e135a0289
ellen.youn <ellen.youn@kakaocorp.com>
2021-04-19 01:14:53 +0900

    [Feature] Add Network Sample Code (#8)

--- app/src/main/java/com/upf/memorytrace_android/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/upf/memorytrace_android/viewmodel/LoginViewModel.kt
@@ -1,8 +1,19 @@
 package com.upf.memorytrace_android.viewmodel
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.upf.memorytrace_android.api.util.MemoryTraceUtils
 import com.upf.memorytrace_android.base.BaseViewModel
+import kotlinx.coroutines.launch
 
 internal class LoginViewModel : BaseViewModel() {
     val name = MutableLiveData("로긘")
+    val api = MemoryTraceUtils.apiService()
+
+    init {
+        viewModelScope.launch {
+            name.value = api.fetchBooks(1)[0].toString()
+        }
+
+    }
 }
