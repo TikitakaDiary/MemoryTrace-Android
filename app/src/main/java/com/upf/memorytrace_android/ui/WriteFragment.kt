bf835cbcacbda8b25cc4d414881a4c74c9a63482
wishJinit <wishjinit@gmail.com>
2021-05-09 03:48:33 +0900

    [Feature] Check READ Permission, Load Gallery (#26)

--- app/src/main/java/com/upf/memorytrace_android/ui/WriteFragment.kt
+++ app/src/main/java/com/upf/memorytrace_android/ui/WriteFragment.kt
@@ -1,11 +1,16 @@
 package com.upf.memorytrace_android.ui
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.core.content.ContextCompat
 import com.upf.memorytrace_android.R
 import com.upf.memorytrace_android.base.BaseFragment
 import com.upf.memorytrace_android.databinding.FragmentWriteBinding
 import com.upf.memorytrace_android.extension.observe
+import com.upf.memorytrace_android.extension.toast
 import com.upf.memorytrace_android.util.TimeUtil
 import com.upf.memorytrace_android.viewmodel.WriteViewModel
 import com.xiaopo.flying.sticker.DrawableSticker
@@ -18,6 +23,7 @@ internal class WriteFragment : BaseFragment<WriteViewModel, FragmentWriteBinding
         super.onActivityCreated(savedInstanceState)
 
         setProperties()
+        observe(viewModel.isLoadGallery) { if (checkReadPermission()) accessGallery() }
         observe(viewModel.isAttachSticker) { attachSticker() }
     }
 
@@ -28,9 +34,38 @@ internal class WriteFragment : BaseFragment<WriteViewModel, FragmentWriteBinding
         }
     }
 
+    private fun checkReadPermission(): Boolean {
+        val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+        val selfPermission = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        )
+        return if (selfPermission != PackageManager.PERMISSION_GRANTED) {
+            toast(NOTICE_DO_NOT_LOAD_GALLERY)
+            requestPermissions(permissions, REQUEST_CODE_READ_EXTERNAL_STORAGE)
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun accessGallery() {
+        val intent = Intent(
+            Intent.ACTION_PICK,
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        )
+        intent.type = "image/*"
+        startActivityForResult(intent, 0)
+    }
+
     private fun attachSticker() {
         val drawable =
             ContextCompat.getDrawable(requireContext(), R.drawable.ic_launcher_foreground)
         binding.stickerView.addSticker(DrawableSticker(drawable))
+    }
+
+    companion object {
+        private const val REQUEST_CODE_READ_EXTERNAL_STORAGE = 100
+        private const val NOTICE_DO_NOT_LOAD_GALLERY = "이미지를 로드하려면 권한이 필요합니다."
     }
 }
--- app/src/main/java/com/upf/memorytrace_android/viewmodel/WriteViewModel.kt
+++ app/src/main/java/com/upf/memorytrace_android/viewmodel/WriteViewModel.kt
@@ -4,7 +4,12 @@ import com.upf.memorytrace_android.base.BaseViewModel
 import com.upf.memorytrace_android.util.LiveEvent
 
 internal class WriteViewModel : BaseViewModel() {
+    val isLoadGallery = LiveEvent<Unit?>()
     val isAttachSticker = LiveEvent<Unit?>()
+
+    fun loadGallery() {
+        isLoadGallery.call()
+    }
 
     fun attachSticker() {
         isAttachSticker.call()
