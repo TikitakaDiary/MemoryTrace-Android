94e66e2835a3e22c8c383c2628a079a3221cd00f
younditt <dhy039@gmail.com>
2021-05-21 12:56:58 +0900

    [feature] Create Book (#19)

--- app/src/main/java/com/upf/memorytrace_android/api/repository/BookRepository.kt
+++ app/src/main/java/com/upf/memorytrace_android/api/repository/BookRepository.kt
@@ -1,23 +1,15 @@
 package com.upf.memorytrace_android.api.repository
 
 import com.upf.memorytrace_android.api.BaseResponse
+import com.upf.memorytrace_android.api.BookResponse
 import com.upf.memorytrace_android.api.util.MemoryTraceUtils
-import com.upf.memorytrace_android.api.model.BookModel
-import com.upf.memorytrace_android.api.model.CreateBookModel
-import com.upf.memorytrace_android.api.util.NetworkState
 
 object BookRepository {
-    suspend fun createBook(createBookModel: CreateBookModel): NetworkState<BaseResponse> {
-        return try {
-            val response = MemoryTraceUtils.apiService().createBook(createBookModel)
-            response.let {
-                NetworkState.Success(it)
-            }
-        } catch (e: Exception) {
-            NetworkState.Failure(e.message ?: "Internet Error")
-        }
 
     private val PAGE_SIZE = 20
+
+    suspend fun createBook(title: String, bgColor: Int, image: String?) {
+        val response = MemoryTraceUtils.apiService().createBook(title, bgColor)
     }
 
     //TODO: error 객체 관리
---
+++ app/src/main/java/com/upf/memorytrace_android/ui/book/CreateBookFragment.kt
@@ -0,0 +1,83 @@
+package com.upf.memorytrace_android.ui.book
+
+import android.os.Bundle
+import android.view.*
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.RecyclerView
+import com.upf.memorytrace_android.R
+import com.upf.memorytrace_android.base.BaseFragment
+import com.upf.memorytrace_android.databinding.FragmentCreateBookBinding
+import com.upf.memorytrace_android.databinding.ItemColorListBinding
+import com.upf.memorytrace_android.extension.observe
+import com.upf.memorytrace_android.util.Colors
+import com.upf.memorytrace_android.util.hideKeyboard
+import com.upf.memorytrace_android.util.visibleOrGone
+import com.upf.memorytrace_android.viewmodel.CreateBookViewModel
+
+internal class CreateBookFragment : BaseFragment<CreateBookViewModel, FragmentCreateBookBinding>() {
+    override val layoutId = R.layout.fragment_create_book
+    override val viewModelClass = CreateBookViewModel::class
+    private val adapter: ColorListAdapter by lazy {
+        ColorListAdapter(Colors.getColors())
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        setupRecyclerView()
+        initViewModel()
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+            binding.title.hideKeyboard()
+        }
+        binding.btnCreate.setOnClickListener { viewModel.createBook(binding.title.text.toString()) }
+    }
+
+    private fun initViewModel() {
+        observe(viewModel.selectedColor) {
+            binding.itemBook.setCardBackgroundColor(it.toInt())
+        }
+        observe(viewModel.createBookSuccess) {
+            //TODO: 리스트에 데이터 업데이트 되도록 확인 및 처리 필요
+            findNavController().popBackStack()
+            binding.title.hideKeyboard()
+        }
+    }
+
+    private fun setupRecyclerView() {
+        binding.list.setHasFixedSize(true)
+        binding.list.adapter = adapter
+    }
+
+
+    private inner class ColorListAdapter(private val colorList: List<Colors>) :
+        RecyclerView.Adapter<ColorListAdapter.ViewHolder>() {
+        override fun getItemCount(): Int = colorList.size
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            return ViewHolder(
+                ItemColorListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            )
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            holder.bind(colorList[position])
+        }
+
+        private inner class ViewHolder(val binding: ItemColorListBinding) :
+            RecyclerView.ViewHolder(binding.root) {
+
+
+            fun bind(color: Colors) {
+                binding.selected.visibleOrGone(viewModel.isSelected(color))
+                Colors.fillCircle(binding.circleColor, color)
+                itemView.setOnClickListener {
+                    viewModel.setSelectedColor(color)
+                    notifyDataSetChanged()
+                }
+            }
+        }
+    }
+
+
+}
