71fd3196984448978c88dc5880f7a03e6a55e897
wishJinit <wishjinit@gmail.com>
2021-06-02 04:32:20 +0900

    [Refactor] Modify View Structure (#26)

--- app/src/main/java/com/upf/memorytrace_android/binding/ViewBindingAdapter.kt
+++ app/src/main/java/com/upf/memorytrace_android/binding/ViewBindingAdapter.kt
@@ -1,6 +1,7 @@
 package com.upf.memorytrace_android.binding
 
 import android.graphics.Bitmap
+import android.view.View
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 
@@ -8,5 +9,13 @@ import androidx.databinding.BindingAdapter
 internal fun setSrcWithBitmap(iv: ImageView, bitmap: Bitmap?) {
     bitmap?.let {
         iv.setImageBitmap(it)
+    }
+}
+
+@BindingAdapter("android:visibility")
+internal fun setVisibility(v: View, isVisible: Boolean) {
+    v.visibility = when (isVisible) {
+        true -> View.VISIBLE
+        false -> View.GONE
     }
 }
