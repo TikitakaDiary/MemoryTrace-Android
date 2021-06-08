fa82b52450bd8c0da6711725143075c4e2c74c6a
younditt <dhy039@gmail.com>
2021-06-08 23:00:09 +0900

    [feature] UI of Member Setting (#24)
    - 일기장 설정 > 멤버 설정 화면 구현

---
+++ app/src/main/java/com/upf/memorytrace_android/ui/MemberSettingFragment.kt
@@ -0,0 +1,28 @@
+package com.upf.memorytrace_android.ui
+
+import android.app.AlertDialog
+import android.os.Bundle
+import androidx.navigation.fragment.findNavController
+import com.upf.memorytrace_android.R
+import com.upf.memorytrace_android.base.BaseFragment
+import com.upf.memorytrace_android.databinding.FragmentBookSettingBinding
+import com.upf.memorytrace_android.databinding.FragmentMypageBinding
+import com.upf.memorytrace_android.extension.toast
+import com.upf.memorytrace_android.util.MemoryTraceConfig
+import com.upf.memorytrace_android.viewmodel.BookSettingViewModel
+import com.upf.memorytrace_android.viewmodel.MypageViewModel
+
+internal class MemberSettingFragment : BaseFragment<BookSettingViewModel, FragmentBookSettingBinding>() {
+    override val layoutId = R.layout.fragment_book_setting
+    override val viewModelClass = BookSettingViewModel::class
+
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+}
---
+++ app/src/main/java/com/upf/memorytrace_android/viewmodel/MemberSettingViewModel.kt
@@ -0,0 +1,18 @@
+package com.upf.memorytrace_android.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import com.upf.memorytrace_android.BuildConfig
+import com.upf.memorytrace_android.base.BaseViewModel
+import com.upf.memorytrace_android.util.MemoryTraceConfig
+
+internal class MemberSettingViewModel : BaseViewModel(){
+    fun onClickMember(){
+
+    }
+    fun onClickCoverEdit(){
+        navDirections.value
+    }
+    fun onClickLeave(){
+
+    }
+}
