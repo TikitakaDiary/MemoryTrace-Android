a281a7862033ce21f89cb54f38a665ea5189c7ef
wishJinit <wishjinit@gmail.com>
2021-06-08 22:39:17 +0900

    [Bug] Delete unused code (#20)

--- app/src/main/java/com/upf/memorytrace_android/viewmodel/DiaryViewModel.kt
+++ app/src/main/java/com/upf/memorytrace_android/viewmodel/DiaryViewModel.kt
@@ -4,26 +4,18 @@ import androidx.lifecycle.MutableLiveData
 import com.upf.memorytrace_android.base.BaseViewModel
 import com.upf.memorytrace_android.ui.diary.DiaryItem
 import com.upf.memorytrace_android.ui.diary.DiaryFragmentDirections
-import com.upf.memorytrace_android.ui.diary.DiaryListItem
 import com.upf.memorytrace_android.ui.diary.DiaryListType
 import com.upf.memorytrace_android.ui.diary.DiaryMonthItem
 import com.upf.memorytrace_android.util.TimeUtil
 import java.util.Calendar
 
 internal class DiaryViewModel : BaseViewModel() {
-    val diaryListItem = MutableLiveData<List<DiaryListItem>>()
-
     val title = MutableLiveData<String>()
     val listType = MutableLiveData<DiaryListType>(DiaryListType.FRAME)
     val diaryOfMonthList = MutableLiveData<List<DiaryMonthItem>>()
     val isMyTurn = true
 
     init {
-        mutableListOf<DiaryListItem>().apply {
-            if (isMyTurn) add(DiaryListItem.WriteButtonItem())
-            diaryListItem.value = this
-        }
-
         diaryOfMonthList.value = (1..11).map {
             val calendar = Calendar.getInstance()
             calendar.add(Calendar.MONTH, -(it / 2))
