bc069b0488faf3e5080a2bd1d848e4b64beac309
younditt <dhy039@gmail.com>
2021-08-23 00:34:23 +0900

    [Bug] Fix color index

--- app/src/main/java/com/upf/memorytrace_android/util/Colors.kt
+++ app/src/main/java/com/upf/memorytrace_android/util/Colors.kt
@@ -8,18 +8,18 @@ import com.upf.memorytrace_android.R
 
 enum class Colors(val id: Int, val color: Long) {
 
-    RED(1, 0xffD54E4E),
-    ORANGE(2, 0xffF59728),
-    YELLOW(3, 0xffFFD84C),
-    GREEN(4, 0xff68C459),
-    OCEAN_BLUE(5, 0xff43CAC2),
-    BLUE(6, 0xff3578DC),
-    DARK_BLUE(7, 0xff2A43C7),
-    PURPLE(8, 0xff8B47D0),
-    PINK(9, 0xffEA7AA6),
-    LIGHT_GRAY(10, 0xffB1B1B1),
-    DARK_GRAY(11, 0xff696969),
-    SYSTEM_GRAY(0, 0xff4f4f4f);
+    RED(0, 0xffD54E4E),
+    ORANGE(1, 0xffF59728),
+    YELLOW(2, 0xffFFD84C),
+    GREEN(3, 0xff68C459),
+    OCEAN_BLUE(4, 0xff43CAC2),
+    BLUE(5, 0xff3578DC),
+    DARK_BLUE(6, 0xff2A43C7),
+    PURPLE(7, 0xff8B47D0),
+    PINK(8, 0xffEA7AA6),
+    LIGHT_GRAY(9, 0xffB1B1B1),
+    DARK_GRAY(10, 0xff696969),
+    SYSTEM_GRAY(11, 0xff4f4f4f);
 
     fun toInt() = color.toInt()
 
