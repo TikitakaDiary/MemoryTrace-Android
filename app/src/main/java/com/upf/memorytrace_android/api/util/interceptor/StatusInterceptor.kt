752437eabe912a4357d9cf790d4f49e3f7b04856
winter223 <link5658@gmail.com>
2022-01-06 21:21:01 +0900

    [Feature][#86] BaseResponse#Deserializer 에러 로깅

--- app/src/main/java/com/upf/memorytrace_android/api/util/interceptor/StatusInterceptor.kt
+++ app/src/main/java/com/upf/memorytrace_android/api/util/interceptor/StatusInterceptor.kt
@@ -7,6 +7,7 @@ import com.upf.memorytrace_android.api.util.StatusError
 import okhttp3.Interceptor
 import okhttp3.Response
 import java.io.IOException
+import java.lang.Exception
 
 class StatusInterceptor : Interceptor {
     @Throws(IOException::class)
@@ -15,10 +16,19 @@ class StatusInterceptor : Interceptor {
         val response = chain.proceed(request)
         //todo: 더 좋은 방법을 찾아볼 것(https://github.com/square/okhttp/issues/1240#issuecomment-330813274)
         val body = response.peekBody(Long.MAX_VALUE)
-        val gson = GsonBuilder().registerTypeAdapter(
-            BaseResponse::class.java,
-            BaseResponse.Deserializer()
-        ).create()
+        val gson = try {
+            GsonBuilder().registerTypeAdapter(
+                BaseResponse::class.java,
+                BaseResponse.Deserializer()
+            ).create()
+        } catch (e: Exception) {
+            val logMessage = "url : ${request.url}, body : ${request.body}, responseBody : $body"
+            FirebaseCrashlytics.getInstance().run {
+                log(logMessage)
+                recordException(e)
+            }
+            return response
+        }
         val data: BaseResponse<*> = gson.fromJson(body.string(), BaseResponse::class.java)
         if (!data.isSuccess) {
             val logMessage = "${data.responseMessage}, url : ${request.url}, body : ${request.body}"
