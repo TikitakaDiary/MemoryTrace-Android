d6f41a32224f26e377e74d13216eb2e5e54445b4
winter223 <link5658@gmail.com>
2022-02-15 13:37:35 +0900

    [#80][Improvement] DiaryFragment 개선
    - 불필요한 파일 제거

--- app/src/main/java/com/upf/memorytrace_android/ui/diary/list/item/DiaryMonthItem.kt
+++
@@ -1,9 +0,0 @@
-package com.upf.memorytrace_android.ui.diary.list.item
-
-import com.upf.memorytrace_android.ui.base.BaseItem
-
-data class DiaryMonthItem(
-    val date: String,
-    val diaryList: List<DiaryItem>,
-    override val itemId: String = date
-) : BaseItem
